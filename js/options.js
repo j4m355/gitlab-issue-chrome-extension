30844d43759461f314c8842bb9290d8738c2fe9a
j4m355 <e@mailjames.com>
2013-12-22 20:29:55 +0000

    connect and store users to localstorage

--- js/options.js
+++ js/options.js
@@ -1,51 +1,54 @@
+// Saves options to localStorage.
+function save_options() {
+  var selectHost = document.getElementById("gitlabHost");
+  var gitlabHost = selectHost.value;
+  console.log(gitlabHost);
+  localStorage["gitlabHost"] = gitlabHost;
 
-;(function($){
-
-	var optionApp = {
-		init: function(){
-			this.container = $('div.container');
-			this._config(localStorage,false);
-			this.config = localStorage;
-			this.initPage();
-		},
-		_config: function(config,reset){
-		//application space
-
-			var c = config = config ? config : {};
-			if(reset){
-				config = {};
-			}
-			//host for connection
-			c['host'] = c['host'] ? c['host'] : 'http://code.dianpingoa.com/';
-			//api alias of the host
-			c['api'] = c['api'] ? c['api'] : 'api/v3/';
-			//user fingerprint
-			c['privateKey'] = c['privateKey'] ? c['privateKey'] : '';
-			//notification refresh frequency
-			c['frequency'] = c['privateKey'] ? c['privateKey'] : '600000';
-			//projects from repositories
-			c['projects'] = c['projects'] ? c['projects'] : [];
+  var selectPrivateKey = document.getElementById("privateKeyv");
+  var privatekey = selectPrivateKey.value;
+  console.log(privatekey);
+  localStorage["privatekey"] = privatekey;
 
 
-			return c;
+  //connect
+  var userReq = new XMLHttpRequest()
+  userReq.open("GET", return_api_url('users'), false);
+  userReq.send(null);
+  localStorage["developers"] = userReq.responseText;
+  console.log(userReq.responseText);
 
-		},
-
-		initPage: function(){
-			var connectInput = $('input.J_connect',this.container), authInput = $('input.J_auth',this.container);
-			connectInput.val(this.config['host']);
-			authInput.val(this.config['privateKey']);
-		}
-
-
-	}
-
-	$(document).ready(function(){
-
-		optionApp.init();
-	})
-	
-	
+  
 
 
-})(jQuery);
+  if(userReq.status == 200)
+  {    
+   document.getElementById("connect").className = "btn btn-default btn-block J_sync btn-success";
+  }
+  else
+  {
+    document.getElementById("connect").className = "btn btn-default btn-block J_sync btn-danger";
+	console.log("Cannot connect - check your credentials");
+  }
+
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var gitlabHost = localStorage["gitlabHost"]; 
+  var privatekey = localStorage["privatekey"]; 
+
+  if(gitlabHost != undefined)
+  {
+  	document.getElementById("gitlabHost").value = gitlabHost;
+  	document.getElementById("privateKeyv").value = privatekey;
+  }
+  document.querySelector('#connect').addEventListener('click', save_options);
+}
+
+function return_api_url(route)
+{
+	return  localStorage["gitlabHost"] + '/api/v3/' + route + '?private_token=' + localStorage["privatekey"];
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
